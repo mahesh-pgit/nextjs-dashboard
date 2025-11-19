75639fcb5f307e96001d1103b3384142c0bf4f27
Mahesh <naravamaheshsaikumar.321@gmail.com>
2025-11-19 18:16:56 +0530

    feat(customers): implement customer filtering with search functionality and update CustomersTable component

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,21 @@
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
 	title: "Customers",
 };
 
-const Page = () => {
-	return <div>Customers Page</div>;
+const Page = async (props: { searchParams?: Promise<{ query?: String }> }) => {
+	const searchParams = await props.searchParams;
+	const query = searchParams?.query || "";
+	const customers = await fetchFilteredCustomers(query.toString());
+
+	return (
+		<main>
+			<CustomersTable customers={customers} />
+		</main>
+	);
 };
 
 export default Page;
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -12,6 +12,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
 	const handleSearch = useDebouncedCallback((term: string) => {
 		const params = new URLSearchParams(searchParams);
 		params.set("page", "1"); // Reset to first page on new search
+
+		if (params.get("page") === "1") {
+			params.delete("page");
+		}
+
 		if (term) {
 			params.set("query", term);
 		} else {
