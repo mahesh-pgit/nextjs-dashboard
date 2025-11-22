05a3ead610ac5a82868aab6644fc9e5d1facfae0
Mahesh <naravamaheshsaikumar.321@gmail.com>
2025-11-22 22:52:12 +0530

    refactor: clean up route and simplify initial SQL query setup

--- app/query/query.ts
+++ app/query/query.ts
@@ -1,21 +1,12 @@
+// rename this file to route.ts to make it routable and test sql queries in development
+// discard changes and do not commit with this file named route.ts unless required
+
 import postgres from "postgres";
-import bcrypt from "bcrypt";
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
-const user = {
-	id: "410544b2-4001-4271-9855-fec4b6a6442a",
-	name: "User",
-	email: "user@nextmail.com",
-	password: "1248163264",
-};
-
-async function reseedUser() {
-	const hashedPassword = await bcrypt.hash(user.password, 10);
+async function testQuery() {
 	const data = await sql`
-		INSERT INTO users (id, name, email, password)
-		VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
-		ON CONFLICT (id) DO NOTHING;
 	`;
 
 	return data;
@@ -23,7 +14,7 @@ async function reseedUser() {
 
 export async function GET() {
 	try {
-		return Response.json(await reseedUser());
+		return Response.json(await testQuery());
 	} catch (error) {
 		return Response.json({ error }, { status: 500 });
 	}
